6a353edc8f3d2f4b3f417aabfd28ea05a4cd6492
javimu06 <javimu06@ucm.es>
2022-04-29 11:49:02 +0200

    Planteamiento ejercicio 2

--- rvr-release1.0/practica2.2/serializacion/ejercicio1.cc
+++ rvr-release1.0/practica2.2/serializacion/ejercicio1.cc
@@ -21,7 +21,13 @@ public:
 
     void to_bin()
     {
-        //
+        alloc_data(sizeof(char) * MAX_NAME + sizeof(int16_t) * 2); //name + pos_x + pos_y
+        char* tmp = _data + sizeof(int32_t);
+        memcpy(tmp, (void*)name, sizeof(char) * MAX_NAME);
+        tmp += sizeof(char) * MAX_NAME;
+        memcpy(tmp, &pos_x, sizeof(int16_t));
+        tmp += sizeof(int16_t);
+        memcpy(tmp, &pos_y, sizeof(int16_t));
     }
 
     int from_bin(char * data)
@@ -32,7 +38,8 @@ public:
 
 
 public:
-    char name[80];
+    static const size_t MAX_NAME = 80;
+    char name[MAX_NAME];
 
     int16_t x;
     int16_t y;
@@ -43,8 +50,14 @@ int main(int argc, char **argv)
     Jugador one_r("", 0, 0);
     Jugador one_w("Player_ONE", 123, 987);
 
+
     // 1. Serializar el objeto one_w
+    int file = open("one_w.txt", O_RDWR | O_TRUNC | O_CREAT);
+    if (file < 0) return -1;
+    one_w.to_bin(); 
     // 2. Escribir la serialización en un fichero
+    write(file, one_w.data(), one_w.size());
+    close(file);
     // 3. Leer el fichero
     // 4. "Deserializar" en one_r
     // 5. Mostrar el contenido de one_r
